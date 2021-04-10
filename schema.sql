3530ff3ab051cc70a1cfcd6476e8c002b9aef48f
Matthew Vanni <mvanni4994@gmail.com>
2021-04-10 08:56:05 -0400

    added database from sql workbench

---
+++ schema.sql
@@ -0,0 +1,38 @@
+DROP DATABASE IF EXISTS employeetracker_db;
+CREATE DATABASE employeetracker_db;
+
+USE employeetracker_db;
+
+CREATE TABLE employee (
+  id INTEGER AUTO_INCREMENT NOT NULL,
+  first_name VARCHAR(30) NOT NULL,
+  last_name VARCHAR(30) NOT NULL,
+  role_id INTEGER,
+  manager_id INTEGER,
+  PRIMARY KEY (id)
+);
+
+CREATE TABLE role (
+  id INTEGER AUTO_INCREMENT NOT NULL,
+  title VARCHAR(30) NOT NULL,
+  salary INTEGER NOT NULL,
+  department_id INTEGER,
+  PRIMARY KEY (id)
+);
+
+CREATE TABLE department (
+  id INTEGER AUTO_INCREMENT NOT NULL,
+  name VARCHAR(30) NOT NULL,
+  PRIMARY KEY (id)
+);
+
+
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES ("Matt", "Vanni", 5, 15);
+
+
+UPDATE people
+SET first_name = "Joe", manager_id = 12
+WHERE id = 4;
+
+SELECT * FROM employee;
