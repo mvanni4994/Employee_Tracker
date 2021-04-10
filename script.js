32e03816732ca82f8652bcb72bfdef4451167e9e
Matthew Vanni <mvanni4994@gmail.com>
2021-04-10 09:18:01 -0400

    added initial inquirer prompt

--- script.js
+++ script.js
@@ -25,3 +25,54 @@ connection.connect(function (err) {
     execDB();
   });
 });
+
+var roles = []
+var managers = []
+var departments = []
+
+
+function askUser() {
+    inquirer
+    .prompt([
+        {
+        name: "action",
+        type: "rawlist",
+        message: "What would you like to do?",
+        choices: [
+            "Add department",
+            "Add role",
+            "Add employee",
+            "Update employee role",
+            "View information",
+            "Delete information",
+            "Exit"
+        ]
+        }
+
+    ]).then(function(answer) {
+        switch (answer.action) {
+        
+            case "Add employee":
+                addEmployee();
+            break;
+            case "Add department":
+                addDepartment();
+            break;
+            case "Add role":
+                addRole();
+            break;
+            case "Update employee role":
+                userUpdate();
+            break;
+            case "View information":
+                userView();
+            break;
+            case "Delete information":
+                userDelete();
+            break;
+            case "Exit":
+                process.exit();
+            break;
+        }
+    })
+}
