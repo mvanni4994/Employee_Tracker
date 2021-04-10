c53a639bad9f1876519d08d19162ffd7aec844ee
Matthew Vanni <mvanni4994@gmail.com>
2021-04-10 09:24:39 -0400

    added functions to connect to DB

--- script.js
+++ script.js
@@ -26,10 +26,72 @@ connection.connect(function (err) {
   });
 });
 
-var roles = []
-var managers = []
-var departments = []
-
+function viewEmp() {
+    var query =
+      "SELECT employee.id, employee.first_name, employee.last_name, role.title, role.salary,";
+    query +=
+      "department.department, employee.manager_id FROM employee LEFT JOIN role ON (employee.role_id = role.id)";
+    query += "LEFT JOIN department ON (role.department_id = department.id)";
+    connection.query(query, function (err, res) {
+      if (err) throw err;
+      console.table(res);
+    });
+  }
+  
+  function viewRoleChoices() {
+    var query = "SELECT id, name FROM role";
+    connection.query(query, function (err, res) {
+      if (err) rej(err);
+      const roleRes = res;
+      const roleArr = [];
+      for (let i = 0; i < roleRes.length; i++) {
+        roleArr.push({
+          name: roleRes[i].name,
+          value: roleRes[i].id,
+          short: roleRes[i].id,
+        });
+      }
+    });
+    return roleArr;
+  }
+  
+  function viewManagerChoices() {
+    return new Promise((res, rej) => {
+      var query =
+        "SELECT employee.id, employee.first_name, employee.last_name FROM employee;";
+      connection.query(query, function (err, res) {
+        if (err) rej(err);
+        const manageRes = res;
+        const managerArr = [];
+        for (let i = 0; i < manageRes.length; i++) {
+          managerArr.push({
+            name: manageRes[i].first_name + " " + manageRes[i].last_name,
+            value: manageRes[i].id,
+            short: manageRes[i].id,
+          });
+        }
+      });
+      res(managerArr);
+    });
+  }
+  
+  async function viewDeptChoices() {
+    var query = "SELECT * FROM department";
+   const choiceList = await connection.query(query, function (err, res) {
+      if (err) throw err;
+      const deptRes = res;
+      const deptArr = deptRes.map((obj) => {
+        return {
+          name: obj.department,
+          value: obj.id,
+          short: obj.id,
+        };
+      });
+      return deptArr;
+    });
+    return choiceList
+  }
+  
 
 function askUser() {
     inquirer
